932166d691fb1276f3dd3c41878759151b715ee0
Rohan Parkes <rparkes@email.com>
2025-11-04 14:29:05 +1100

    Wired up routing.

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,46 +1,14 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Repository } from './models/repository';
-import { Product } from './models/product.model';
-import { Supplier } from './models/supplier.model';
-import { ProductTableComponent } from './structure/productTable.component';
-import { CategoryFilterComponent } from './structure/categoryFilter.component';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   imports: [
-    ProductTableComponent,
-    CategoryFilterComponent
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  private repo: Repository = inject(Repository);
-  private errorsChanged: Subscription = new Subscription();
+export class AppComponent {
 
-  title = 'SportsStore';
-  product: Product | undefined = undefined;
-  products: Product[] = [];
-  supplier: Supplier | undefined = undefined;;
-  suppliers: Supplier[] = [];
-  errorMessage = '';
-
-  ngOnInit() {
-    this.errorsChanged = this.repo.errorsChanged.subscribe(message => {
-      let err = '';
-      Object.keys(message).forEach(key => {
-        if (key !== '') {
-          err += `${message[key].join('\n')}`;
-        }
-      });
-      this.errorMessage = err;
-    });
-
-    // this.repo.getProducts(true);
-  }
-
-  ngOnDestroy() {
-    this.errorsChanged.unsubscribe();
-  }
 }
--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductTableComponent } from './structure/productTable.component';
+import { ProductDetailComponent } from './structure/productDetail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "table", component: ProductTableComponent },
+  { path: "detail", component: ProductDetailComponent },
+  { path: "", component: ProductTableComponent }
+];
