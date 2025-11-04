af7eb70d7ffee1e928d0e3cd8afaad7cf3ab6221
Rohan Parkes <rparkes@email.com>
2025-11-04 22:19:32 +1100

    Added star ratings.

--- ClientApp/src/app/store/productList.component.ts
+++ ClientApp/src/app/store/productList.component.ts
@@ -1,38 +1,32 @@
-import { Component, inject, OnDestroy, OnInit } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { CurrencyPipe } from "@angular/common";
-import { Subscription } from "rxjs";
 import { Repository } from "../models/repository";
 import { Product } from "../models/product.model";
+import { RatingsComponent } from "./ratings.component";
 
 @Component({
   selector: "store-product-list",
   imports: [
-    CurrencyPipe
+    CurrencyPipe,
+    RatingsComponent
   ],
   templateUrl: "productList.component.html"
 })
-export class ProductListComponent implements OnInit, OnDestroy {
-  private productsChanged: Subscription = new Subscription();
+export class ProductListComponent implements OnInit {
   private repo: Repository = inject(Repository);
 
   constructor() { }
 
   ngOnInit() {
-    this.productsChanged = this.repo.productsChanged.subscribe({
-      next: (productList) => {
-        // this.products = productList;
-      },
-      error: () => { }
-    }
-    );
-
     this.repo.getProductsAsync();
   }
 
   get products(): Product[] {
     if (this.repo.getProductsCached() != null && this.repo.getProductsCached().length > 0) {
       let pageIndex = (this.repo.paginationObject.currentPage - 1) * this.repo.paginationObject.productsPerPage;
-      return this.repo.getProductsCached().slice(pageIndex, pageIndex + this.repo.paginationObject.productsPerPage);
+      return this.repo
+        .getProductsCached()
+        .slice(pageIndex, pageIndex + this.repo.paginationObject.productsPerPage);
     }
 
     return [];
@@ -40,9 +34,5 @@ export class ProductListComponent implements OnInit, OnDestroy {
 
   addToCart(product: Product) {
 
-  }
-
-  ngOnDestroy() {
-    this.productsChanged.unsubscribe();
   }
 }
