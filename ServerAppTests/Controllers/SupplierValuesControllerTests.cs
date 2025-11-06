5f6496dfb63d9aaf5d75b616efe272ef67b2ea7c
Rohan Parkes <rparkes@email.com>
2025-11-06 14:02:42 +1100

    Added Unit Tests.

---
+++ ServerAppTests/Controllers/SupplierValuesControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerAppTests.Controllers
+{
+  [TestClass()]
+  public class SupplierValuesControllerTests
+  {
+    [TestMethod()]
+    public void CreateSupplierTest()
+    {
+      Assert.Fail();
+    }
+  }
+}
