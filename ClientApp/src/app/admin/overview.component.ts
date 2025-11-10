fedf7393f69b52fa08558f7138f5e5716f1388a2
Rohan Parkes <rparkes@email.com>
2025-11-10 18:50:45 +1100

    Fixed admin navigation.

--- ClientApp/src/app/admin/overview.component.ts
+++ ClientApp/src/app/admin/overview.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Order } from '../models/order.model';
 import { Product } from '../models/product.model';
 import { Repository } from '../models/repository';
 
 @Component({
   templateUrl: 'overview.component.html',
+  imports: [RouterLink],
 })
 export class OverviewComponent {
   private repo: Repository = inject(Repository);
